7a70b58db4385d60bf03d1fd28c256abd386eb08
Rakesh H S <rakhs@cisco.com>
2019-07-30 09:13:08 +0530

    Move examples to folder

--- aws/generator.py
+++ aws/generator.py
@@ -1,17 +1,17 @@
 from aws.cloud_front import CloudFront
 from aws.rds import *
 from terrascript import provider
-from aws import example_elb_asg
-from aws import example_elb_asg_ebs
-from aws import example_alb_asg
+from aws.examples import elb_asg
+from aws.examples import elb_asg_ebs
+from aws.examples import alb_asg
 
 
 def generate_terraform(ts, awsapi, json_input):
     ts += provider('aws',region='us-east-1')
     #xyz= RdsInstance(awsapi, json_input).get_instance()
     #ts.add(xyz)
-    #example_elb_asg.ExampleElbAsg(ts, awsapi, json_input).add_instance()
-    #example_elb_asg_ebs.ExampleElbAsgEbs(ts, awsapi, json_input).add_instance()
+    #elb_asg.ExampleElbAsg(ts, awsapi, json_input).add_instance()
+    #elb_asg_ebs.ExampleElbAsgEbs(ts, awsapi, json_input).add_instance()
 
-    example_alb_asg.ExampleElbAsg(ts, awsapi, json_input).add_instance()
+    alb_asg.ExampleElbAsg(ts, awsapi, json_input).add_instance()
     return ts
