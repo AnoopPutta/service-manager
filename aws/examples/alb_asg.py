8ddcac160193990f4b9e00ff709ef1671e0c911c
Brijesh <brip@cisco.com>
2019-07-31 17:40:50 +0530

    Update example alb deployment with infra changes and sample json

--- aws/examples/alb_asg.py
+++ aws/examples/alb_asg.py
@@ -3,6 +3,11 @@ from aws import alb
 from aws import alb_listener
 from aws import asg
 from aws import alb_target_group
+from aws import vpc
+from aws import internet_gateway as igw
+from aws import route_table
+from aws import route
+from aws import subnet
 
 
 class ExampleElbAsg(object):
@@ -14,6 +19,85 @@ class ExampleElbAsg(object):
 
     def add_instance(self):
 
+        owner = "test"
+        stack = "test-stack"
+
+        default_tags = {
+            "Owner": owner,
+            "Stack": stack
+        }
+
+        # input json for VPC
+        self.input_json = {
+            "name": 'main',
+            "cidr_block": '10.10.0.0/16',
+            "tags": default_tags
+        }
+        main_vpc = vpc.Vpc(self.aws_resource, self.input_json).add_instance()
+        self.ts.add(main_vpc)
+
+        # input json for internet gateway
+        self.input_json = {
+            "name": 'public',
+            "vpc_id": main_vpc.id,
+            "tags": default_tags
+        }
+        public_igw = igw.InternetGateway(self.aws_resource, self.input_json).add_instance()
+        self.ts.add(public_igw)
+
+        # input json for public route table
+        self.input_json = {
+            "name": 'public',
+            "vpc_id": main_vpc.id,
+            "tags": default_tags
+        }
+        public_rtb = route_table.RouteTable(self.aws_resource, self.input_json).add_instance()
+        self.ts.add(public_rtb)
+
+        # input json for internet gateway route
+        self.input_json = {
+            "name": 'igw-route',
+            "route_table_id": public_rtb.id,
+            "destination_cidr_block": '0.0.0.0/0',
+            "gateway_id": public_igw.id
+        }
+        igw_route = route.Route(self.aws_resource, self.input_json).add_instance()
+        self.ts.add(public_rtb)
+
+        public_subnet_cidrs = ['10.10.1.0/24', '10.10.2.0/24']
+        private_subnet_cidrs = ['10.10.11.0/24', '10.10.12.0/24']
+        availability_zones = ['us-east-1a', 'us-east-1b']
+
+        public_subnets = []
+        private_subnets = []
+        for i in range(0, len(public_subnet_cidrs)):
+            # input json for public subnets
+            self.input_json = {
+                "name": 'public_subnet_az'+str(i),
+                "vpc_id": main_vpc.id,
+                "cidr_block": public_subnet_cidrs[i],
+                "availability_zone": availability_zones[i],
+                "map_public_ip_on_launch": True,
+                "tags": default_tags
+            }
+            public_subnet = subnet.Subnet(self.aws_resource, self.input_json).add_instance()
+            self.ts.add(public_subnet)
+            public_subnets.append(public_subnet)
+
+        for i in range(0, len(private_subnet_cidrs)):
+            # input json for public subnets
+            self.input_json = {
+                "name": 'private_subnet_az'+str(i),
+                "vpc_id": main_vpc.id,
+                "cidr_block": private_subnet_cidrs[i],
+                "availability_zone": availability_zones[i],
+                "map_public_ip_on_launch": False,
+                "tags": self.input_json["tags"]
+            }
+            private_subnet = subnet.Subnet(self.aws_resource, self.input_json).add_instance()
+            self.ts.add(private_subnet)
+            private_subnets.append(private_subnet)
+
         user_data = """#!/bin/bash
         cat > index.html <<EOF
         <h1>Hello, World</h1>
@@ -22,14 +106,16 @@ class ExampleElbAsg(object):
         """
 
         server_port = 8080
-        vpc_id = "vpc-74b24210"
+        vpc_id = main_vpc.id
         instance_type = "t2.micro"
-        subnets = ['subnet-054c3ae2dd9d17e98', 'subnet-03ebea26038df39b3']
-        name = "rakhs-test"
-        tags = {"owner": "rakhs"}
+        subnets = [
+            public_subnets[0].id,
+            public_subnets[1].id
+        ]
+        name = stack
 
         elb_sg = self.aws_resource.aws_security_group(
-            name+"-elb", name='{}-elb'.format(name), vpc_id=vpc_id, tags=tags
+            name+"-elb", name='{}-elb'.format(name), vpc_id=vpc_id, tags=default_tags
         )
 
         elb_sg_ingress_rule = self.aws_resource.aws_security_group_rule(
@@ -48,7 +134,7 @@ class ExampleElbAsg(object):
 
         launch_config_sg = self.aws_resource.aws_security_group(
             name+"-inst", name='{}-instance'.format(name),
-            vpc_id=vpc_id, tags=tags
+            vpc_id=vpc_id, tags=default_tags
         )
 
         launch_config_sg_egress_rule = self.aws_resource.aws_security_group_rule(
@@ -80,11 +166,8 @@ class ExampleElbAsg(object):
         self.input_json={
             "name": name,
             "security_groups": [elb_sg.id],
-            "subnets": [
-                "subnet-054c3ae2dd9d17e98",
-                "subnet-03ebea26038df39b3"
-            ],
-            "tags": tags
+            "subnets": subnets,
+            "tags": default_tags
         }
 
         application_lb = alb.ALB(self.aws_resource, self.input_json).add_instance()
@@ -151,90 +234,117 @@ class ExampleElbAsg(object):
   },
   "resource": {
     "aws_alb": {
-      "rakhs-test": {
-        "name": "rakhs-test",
+      "test-stack": {
+        "name": "test-stack",
         "security_groups": [
-          "${aws_security_group.rakhs-test-elb.id}"
+          "${aws_security_group.test-stack-elb.id}"
         ],
         "subnets": [
-          "subnet-054c3ae2dd9d17e98",
-          "subnet-03ebea26038df39b3"
+          "${aws_subnet.public_subnet_az0.id}",
+          "${aws_subnet.public_subnet_az1.id}"
         ],
         "tags": {
-          "owner": "rakhs"
+          "Owner": "test",
+          "Stack": "test-stack"
         }
       }
     },
     "aws_alb_listener": {
-      "rakhs-test": {
+      "test-stack": {
         "default_action": {
-          "target_group_arn": "${aws_alb_target_group.rakhs-test.arn}",
+          "target_group_arn": "${aws_alb_target_group.test-stack.arn}",
           "type": "forward"
         },
-        "load_balancer_arn": "${aws_alb.rakhs-test.arn}",
+        "load_balancer_arn": "${aws_alb.test-stack.arn}",
         "port": 8080,
         "protocol": "HTTP"
       }
     },
     "aws_alb_target_group": {
-      "rakhs-test": {
-        "name": "rakhs-test",
+      "test-stack": {
+        "name": "test-stack",
         "port": 8080,
         "protocol": "HTTP",
-        "vpc_id": "vpc-74b24210"
+        "vpc_id": "${aws_vpc.main.id}"
       }
     },
     "aws_autoscaling_attachment": {
-      "rakhs-test": {
-        "alb_target_group_arn": "${aws_alb_target_group.rakhs-test.arn}",
-        "autoscaling_group_name": "${aws_autoscaling_group.rakhs-test.id}"
+      "test-stack": {
+        "alb_target_group_arn": "${aws_alb_target_group.test-stack.arn}",
+        "autoscaling_group_name": "${aws_autoscaling_group.test-stack.id}"
       }
     },
     "aws_autoscaling_group": {
-      "rakhs-test": {
-        "launch_configuration": "${aws_launch_configuration.rakhs-test.id}",
+      "test-stack": {
+        "launch_configuration": "${aws_launch_configuration.test-stack.id}",
         "max_size": 3,
         "min_size": 2,
         "tag": [
           {
             "key": "Name",
             "propagate_at_launch": true,
-            "value": "rakhs-test"
+            "value": "test-stack"
           }
         ],
         "vpc_zone_identifier": [
-          "subnet-054c3ae2dd9d17e98",
-          "subnet-03ebea26038df39b3"
+          "${aws_subnet.public_subnet_az0.id}",
+          "${aws_subnet.public_subnet_az1.id}"
         ]
       }
     },
+    "aws_internet_gateway": {
+      "public": {
+        "lifecycle": {
+          "create_before_destroy": true
+        },
+        "tags": {
+          "Owner": "test",
+          "Stack": "test-stack"
+        },
+        "vpc_id": "${aws_vpc.main.id}"
+      }
+    },
     "aws_launch_configuration": {
-      "rakhs-test": {
+      "test-stack": {
         "image_id": "ami-0b898040803850657",
         "instance_type": "t2.micro",
         "lifecycle": {
           "create_before_destroy": true
         },
         "security_groups": [
-          "${aws_security_group.rakhs-test-elb.id}"
+          "${aws_security_group.test-stack-elb.id}"
         ],
         "user_data": "#!/bin/bash\n        cat > index.html <<EOF\n        <h1>Hello, World</h1>\n        EOF\n        nohup busybox httpd -f -p \"{}\" &\n        "
       }
     },
+    "aws_route_table": {
+      "public": {
+        "lifecycle": {
+          "create_before_destroy": true
+        },
+        "tags": {
+          "Owner": "test",
+          "Stack": "test-stack"
+        },
+        "vpc_id": "${aws_vpc.main.id}"
+      }
+    },
     "aws_security_group": {
-      "rakhs-test-elb": {
-        "name": "rakhs-test-elb",
+      "test-stack-elb": {
+        "name": "test-stack-elb",
         "tags": {
-          "owner": "rakhs"
+          "Owner": "test",
+          "Stack": "test-stack"
         },
-        "vpc_id": "vpc-74b24210"
+        "vpc_id": "${aws_vpc.main.id}"
       },
-      "rakhs-test-inst": {
-        "name": "rakhs-test-instance",
+      "test-stack-inst": {
+        "name": "test-stack-instance",
         "tags": {
-          "owner": "rakhs"
+          "Owner": "test",
+          "Stack": "test-stack"
         },
-        "vpc_id": "vpc-74b24210"
+        "vpc_id": "${aws_vpc.main.id}"
       }
     },
     "aws_security_group_rule": {
@@ -244,7 +354,7 @@ class ExampleElbAsg(object):
         ],
         "from_port": 0,
         "protocol": "-1",
-        "security_group_id": "${aws_security_group.rakhs-test-elb.id}",
+        "security_group_id": "${aws_security_group.test-stack-elb.id}",
         "to_port": 0,
         "type": "egress"
       },
@@ -254,7 +364,7 @@ class ExampleElbAsg(object):
         ],
         "from_port": 8080,
         "protocol": "tcp",
-        "security_group_id": "${aws_security_group.rakhs-test-elb.id}",
+        "security_group_id": "${aws_security_group.test-stack-elb.id}",
         "to_port": 8080,
         "type": "ingress"
       },
@@ -264,9 +374,77 @@ class ExampleElbAsg(object):
         ],
         "from_port": 8080,
         "protocol": "tcp",
-        "security_group_id": "${aws_security_group.rakhs-test-inst.id}",
+        "security_group_id": "${aws_security_group.test-stack-inst.id}",
         "to_port": 8080,
         "type": "ingress"
+      }
+    },
+    "aws_subnet": {
+      "private_subnet_az0": {
+        "availability_zone": "us-east-1a",
+        "cidr_block": "10.10.11.0/24",
+        "lifecycle": {
+          "create_before_destroy": true
+        },
+        "map_public_ip_on_launch": false,
+        "tags": {
+          "Owner": "test",
+          "Stack": "test-stack"
+        },
+        "vpc_id": "${aws_vpc.main.id}"
+      },
+      "private_subnet_az1": {
+        "availability_zone": "us-east-1b",
+        "cidr_block": "10.10.12.0/24",
+        "lifecycle": {
+          "create_before_destroy": true
+        },
+        "map_public_ip_on_launch": false,
+        "tags": {
+          "Owner": "test",
+          "Stack": "test-stack"
+        },
+        "vpc_id": "${aws_vpc.main.id}"
+      },
+      "public_subnet_az0": {
+        "availability_zone": "us-east-1a",
+        "cidr_block": "10.10.1.0/24",
+        "lifecycle": {
+          "create_before_destroy": true
+        },
+        "map_public_ip_on_launch": true,
+        "tags": {
+          "Owner": "test",
+          "Stack": "test-stack"
+        },
+        "vpc_id": "${aws_vpc.main.id}"
+      },
+      "public_subnet_az1": {
+        "availability_zone": "us-east-1b",
+        "cidr_block": "10.10.2.0/24",
+        "lifecycle": {
+          "create_before_destroy": true
+        },
+        "map_public_ip_on_launch": true,
+        "tags": {
+          "Owner": "test",
+          "Stack": "test-stack"
+        },
+        "vpc_id": "${aws_vpc.main.id}"
+      }
+    },
+    "aws_vpc": {
+      "main": {
+        "cidr_block": "10.10.0.0/16",
+        "enable_dns_hostnames": true,
+        "enable_dns_support": true,
+        "lifecycle": {
+          "create_before_destroy": true
+        },
+        "tags": {
+          "Owner": "test",
+          "Stack": "test-stack"
+        }
       }
     }
   }
